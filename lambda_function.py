6b7fcb155b864fd31d5174a6efacac6e3fdb66d1
Mitchell Broome <mbroome@centos7.attlocal.net>
2019-05-22 21:23:30 -0400

    Working via api gateway

--- lambda_function.py
+++ lambda_function.py
@@ -16,24 +16,21 @@ client = MongoClient(os.environ['MONGODB_ATLAS_CLUSTER_URI'])
 # Set database
 db = client.metrics
 
-class DateTimeEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, datetime):
-            return o.isoformat()
-
-        return json.JSONEncoder.default(self, o)
-
 def lambda_handler(event, context):
+   data = {}
    logger.info("Received event: " + json.dumps(event, indent=2))
-
+   if 'body' in event:
+      data = json.loads(event['body'])
+   else:
+      data = event
+      
    collection = db.metrics
-   logger.info(json.dumps(event))
+   logger.info(json.dumps(data))
 
    response = collection.update_one(
-                 { "metric": event['metric'] },
-                 { "$push": { "points": { "timestamp": datetime.datetime.utcnow(), "value": event['value'] } } },
+                 { "metric": data['metric'] },
+                 { "$push": { "points": { "timestamp": datetime.datetime.utcnow(), "value": data['value'] } } },
                  True)
 
-   #return json.loads(json.dumps(response, cls=DateTimeEncoder))
    return(True)
-      
+
